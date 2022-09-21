8cdda20683c0bc83cc69e94a1c204d939b26c6fa
rtran1029 <103476583+rtran1029@users.noreply.github.com>
2022-09-20 21:31:46 -0400

    setting up db and config connection

---
+++ server.js
@@ -0,0 +1,42 @@
+const session = require('express-session');
+const path = require('path');
+const express = require("express");
+const exphbs = require('express-handlebars');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const sequelize = require("./config/connection");
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+  secret: 'Super secret secret',
+  cookie: { maxAge: 10 * 60 * 1000 },
+  resave: false,
+  rolling: true,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+app.use(session(sess));
+
+const helpers = require('./utils/helpers');
+
+const hbs = exphbs.create({ helpers });
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.use(require('./controllers/'));
+
+// turn on connection to db and server
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log("Now listening"));
+});
