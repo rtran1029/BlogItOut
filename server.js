fd98281033c6a9f28d3662baeb017e3091ad89db
rtran1029 <103476583+rtran1029@users.noreply.github.com>
2022-09-20 23:13:37 -0400

    debuging

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const express = require("express");
 const exphbs = require('express-handlebars');
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+// const PORT = process.env.PORT || 3001;
 
 const sequelize = require("./config/connection");
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
@@ -37,6 +37,13 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(require('./controllers/'));
 
 // turn on connection to db and server
-sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log("Now listening"));
+// var PORT = process.env.PORT || 3000;
+// db.sequelize.sync().then(function() {
+//   app.listen(PORT, function() {
+//     console.log("app listening on port " + PORT);
+//   });
+// });
+
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
